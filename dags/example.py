6d533a91a415022d0b45492cbe586d085ad20c6c
ashma <ashmaal@metrodata.co.id>
2025-01-11 21:53:54 +0700

    test no 4

--- dags/example.py
+++ dags/example.py
@@ -3,3 +3,7 @@
 # 1
 # 2
 # 3 test not using TARGET_SUBS
+# 4 Removed TARGET_BUCKET Substitution:
+#   The bucket selection logic is now fully handled in the script using if conditions based on $BRANCH_NAME.
+#   Dynamic Bucket Resolution:
+#   The TARGET_BUCKET variable is assigned dynamically in the script, removing the need for any substitution.
