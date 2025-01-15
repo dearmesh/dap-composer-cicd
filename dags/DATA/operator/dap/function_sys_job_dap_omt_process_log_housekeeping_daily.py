109eef86dd93f61a51271bd0326c5acb2fe918b5
ashma <ashmaal@metrodata.co.id>
2025-01-15 17:25:17 +0700

    replaced 2 dags files

--- dags/DATA/operator/dap/function_sys_job_dap_omt_process_log_housekeeping_daily.py
+++ dags/DATA/operator/dap/function_sys_job_dap_omt_process_log_housekeeping_daily.py
@@ -60,4 +60,32 @@ class Function():
                         print('task instance :' + task_instance.task_id)
                         raise Exception("Task {} failed. Failing this DAG run".format(task_instance.task_id))
 
+
+    def get_variable_omt_master_job(self,**kwargs):
+        
+        # Initiate Connection Hook
+        bigquery_hook = BigQueryHook(gcp_conn_id='google_cloud_default', delegate_to=None, use_legacy_sql=False,location=self.AIRFLOW_VAR_DAP_LOCATION)
+        
+        # get_first -> list/ tuple
+        variable_omt_master_job = bigquery_hook.get_first(sql=kwargs['query_get_var_{}'.format( "_".join(kwargs['task_instance'].task_id.split("_")[5:]))])
+
+        kwargs['ti'].xcom_push(key='job_name_{}'.format( "_".join(kwargs['task_instance'].task_id.split("_")[5:])), value=variable_omt_master_job[0])
+        kwargs['ti'].xcom_push(key='table_name_{}'.format( "_".join(kwargs['task_instance'].task_id.split("_")[5:])), value=variable_omt_master_job[1])
+        kwargs['ti'].xcom_push(key='query_parameter_{}'.format( "_".join(kwargs['task_instance'].task_id.split("_")[5:])), value=variable_omt_master_job[2])
+        kwargs['ti'].xcom_push(key='execution_date_{}'.format( "_".join(kwargs['task_instance'].task_id.split("_")[5:])), value=variable_omt_master_job[3])
+        kwargs['ti'].xcom_push(key='status_run_execution_date_{}'.format( "_".join(kwargs['task_instance'].task_id.split("_")[5:])), value=variable_omt_master_job[4])
+        kwargs['ti'].xcom_push(key='category_{}'.format( "_".join(kwargs['task_instance'].task_id.split("_")[5:])), value=variable_omt_master_job[5])
+        kwargs['ti'].xcom_push(key='enable_flag_{}'.format( "_".join(kwargs['task_instance'].task_id.split("_")[5:])), value=variable_omt_master_job[6])
+        kwargs['ti'].xcom_push(key='prioritas_{}'.format( "_".join(kwargs['task_instance'].task_id.split("_")[5:])), value=variable_omt_master_job[7])
+        kwargs['ti'].xcom_push(key='source_gcs_folder_{}'.format( "_".join(kwargs['task_instance'].task_id.split("_")[5:])), value=variable_omt_master_job[8])
+        kwargs['ti'].xcom_push(key='archive_gcs_folder_{}'.format( "_".join(kwargs['task_instance'].task_id.split("_")[5:])), value=variable_omt_master_job[9])
+        kwargs['ti'].xcom_push(key='retention_file_day_{}'.format( "_".join(kwargs['task_instance'].task_id.split("_")[5:])), value=variable_omt_master_job[10])
+        kwargs['ti'].xcom_push(key='retention_table_day_{}'.format( "_".join(kwargs['task_instance'].task_id.split("_")[5:])), value=variable_omt_master_job[11])
+        kwargs['ti'].xcom_push(key='query_create_table_{}'.format( "_".join(kwargs['task_instance'].task_id.split("_")[5:])), value=variable_omt_master_job[12])
+        kwargs['ti'].xcom_push(key='target_table_name_{}'.format( "_".join(kwargs['task_instance'].task_id.split("_")[5:])), value=variable_omt_master_job[13])
+        kwargs['ti'].xcom_push(key='table_sp_name_{}'.format( "_".join(kwargs['task_instance'].task_id.split("_")[5:])), value=variable_omt_master_job[14])
+        kwargs['ti'].xcom_push(key='sts_job_name_{}'.format( "_".join(kwargs['task_instance'].task_id.split("_")[5:])), value=variable_omt_master_job[15])
+        kwargs['ti'].xcom_push(key='flag_send_to_sandbox_{}'.format( "_".join(kwargs['task_instance'].task_id.split("_")[5:])), value=variable_omt_master_job[16])
+
+
     
